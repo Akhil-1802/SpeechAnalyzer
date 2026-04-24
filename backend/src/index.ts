d8c25ec305284f47e6a0de23a869a0ef1139f4b5
Akhil-1802 <akhilmaindola18@gmail.com>
2026-04-24 21:38:53 +0530

    project init

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+
+
+const app = express()
+
+
+app.get('/topic' , (req : Request , res : Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
