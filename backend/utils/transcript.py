d85f7c578e01e9755f1ae92a4206b8d29b2e414f
Akhil-1802 <akhilmaindola18@gmail.com>
2026-07-18 22:26:37 +0530

    whisper model changed

--- backend/utils/transcript.py
+++ backend/utils/transcript.py
@@ -6,11 +6,7 @@ def audioTotext(filePath: str):
     global model
 
     if model is None:
-        model = WhisperModel(
-            "base",
-            device="cpu",
-            compute_type="int8"
-        )
+        model = WhisperModel("tiny", device="cpu", compute_type="int8")
 
     segments, info = model.transcribe(filePath)
 
