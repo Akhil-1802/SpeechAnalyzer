ce39b472e3f0ae526a567c1cf72dbf1a56345e44
Akhil-1802 <akhilmaindola18@gmail.com>
2026-07-18 21:33:45 +0530

    testing

--- backend/utils/transcript.py
+++ backend/utils/transcript.py
@@ -1,12 +1,6 @@
-from faster_whisper import WhisperModel
-model = WhisperModel("base", device="cpu", compute_type="int8")
+# from faster_whisper import WhisperModel
+# model = WhisperModel("base", device="cpu", compute_type="int8")
 def audioTotext(filePath : str) -> str:
 
 
-    segments, info = model.transcribe(filePath)
-
-    transcript = ""
-
-    for segment in segments:
-        transcript += segment.text + " "
-    return transcript
+    return "test"
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://speechanalyzer-5wes.onrender.com";
 
 export default API_BASE;
