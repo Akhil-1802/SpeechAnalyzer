c21511a36948d47a3a5ca7cc8338b6e668d75771
Akhil-1802 <akhilmaindola18@gmail.com>
2026-07-18 21:52:31 +0530

    testing

--- backend/utils/transcript.py
+++ backend/utils/transcript.py
@@ -1,6 +1,21 @@
-# from faster_whisper import WhisperModel
-# model = WhisperModel("base", device="cpu", compute_type="int8")
-def audioTotext(filePath : str) -> str:
+from faster_whisper import WhisperModel
 
+model = None
 
-    return "test"
+def audioTotext(filePath: str):
+    global model
+
+    if model is None:
+        model = WhisperModel(
+            "base",
+            device="cpu",
+            compute_type="int8"
+        )
+
+    segments, info = model.transcribe(filePath)
+
+    transcript = ""
+    for segment in segments:
+        transcript += segment.text + " "
+
+    return transcript
